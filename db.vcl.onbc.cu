08795322bee47f97104d2fade8f2afd0f106cae5
Luis Miguel <52539809+luisitohr@users.noreply.github.com>
2024-12-31 15:15:09 -0500

    Create db.vcl.onbc.cu

---
+++ db.vcl.onbc.cu
@@ -0,0 +1,11 @@
+$TTL    604800
+@       IN      SOA     dc.vcl.onbc.cu. root.vcl.onbc.cu. (
+                              2         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
+
+@       IN      NS      dc.vcl.onbc.cu.
+@       IN      A       172.22.1.3
+dc      IN      A       172.22.1.3
